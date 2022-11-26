cd9bd86c97e162e3f32f59deb406b9b3de5c2d20
Marvin Parada <hello@marvin.dev>
2022-11-25 23:23:05 -0800

    Add Vitest.

---
+++ src/views/DashboardView.spec.ts
@@ -0,0 +1,10 @@
+import { describe, expect, it } from 'vitest'
+import { mount } from '@vue/test-utils'
+import DashboardView from './DashboardView.vue'
+
+describe('DashboardView.vue', () => {
+  it('should render the name and version.', () => {
+    const wrapper = mount(DashboardView)
+    expect(wrapper.text()).toContain('Finnanza v')
+  })
+})
