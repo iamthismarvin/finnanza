4541337f97050ab0f1d5fb444219f6adcf7e5022
Marvin Parada <hello@marvin.dev>
2022-11-27 17:10:18 -0800

    Add unit test change.

--- src/views/DashboardView.spec.ts
+++ src/views/DashboardView.spec.ts
@@ -5,6 +5,7 @@ import DashboardView from './DashboardView.vue'
 describe('DashboardView.vue', () => {
   it('should render the name and version.', () => {
     const wrapper = mount(DashboardView)
+    // expect(wrapper.text()).toContain('Finnanza v')
     expect(wrapper.text()).toContain('Finnanza v')
   })
 })
